0992e0ec3b64058c09754c6a26f4531f1e0c9672
Stephan Smith <hello@stephan-smith.com>
2018-07-24 14:03:19 -0500

    Update version number for release

--- cwp-profile.php
+++ cwp-profile.php
@@ -16,7 +16,7 @@
  * Plugin Name:       CedarWP Profile
  * Plugin URI:        https://studiocedar.com/
  * Description:
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Studio Cedar
  * Author URI:        https://studiocedar.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'PLUGIN_NAME_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
